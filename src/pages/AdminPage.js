207c6e34a125679743bc5b09178630d080309c5f
bahaa2003 <77056859+bahaa2003@users.noreply.github.com>
2025-01-22 23:49:22 +0200

    Update AdminPage.js

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom'; // استيراد Link من react-router-dom
 
 const AdminPage = () => {
     const [formData, setFormData] = useState({
