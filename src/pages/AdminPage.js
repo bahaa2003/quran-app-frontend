0346ad771b03d358d3919e2276548edaf1480c31
bahaa2003 <77056859+bahaa2003@users.noreply.github.com>
2025-01-23 00:36:30 +0200

    Update AdminPage.js

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -72,11 +72,11 @@ const AdminPage = () => {
     const uploadFileToCloudinary = async (file, resourceType = 'auto') => {
         const formData = new FormData();
         formData.append('file', file);
-        formData.append('upload_preset', 'quran_app_preset');
+        formData.append('upload_preset', process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET);
 
         try {
             const response = await fetch(
-                `https://api.cloudinary.com/v1_1/df7h4gxaf/${resourceType}/upload`,
+                `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/${resourceType}/upload`,
                 {
                     method: 'POST',
                     body: formData,
